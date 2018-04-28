8d12d136bdab0c9abf88b50aca6a5c8e0b7b6a84
Preksha Kasatwar <preksha726@gmail.com>
2018-04-27 23:06:42 -0500

    Add files via upload

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express =  require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://preksha:perky@ds161539.mlab.com:61539/mytasklist_preksha', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-    res.send('TASK API');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+            res.json(tasks);
+    });
 
 });
 
